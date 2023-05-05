9eeee4a066b0b19802e9fdd8c5871274e24242c4
Jesse Taube <Mr.Bossman075@gmail.com>
2023-05-04 21:37:33 -0400

    adjtimex

--- libc/sysdeps/linux/common/adjtimex.c
+++ libc/sysdeps/linux/common/adjtimex.c
@@ -10,7 +10,14 @@
 #include <sys/timex.h>
 
 
+#if defined __NR_clock_adjtime64 && !defined __NR_adjtimex
+int adjtimex(struct timex *buf)
+{
+	return INLINE_SYSCALL(clock_adjtime64, 2, CLOCK_REALTIME, buf);
+}
+#else
 _syscall1(int, adjtimex, struct timex *, buf)
+#endif
 libc_hidden_def(adjtimex)
 weak_alias(adjtimex,__adjtimex)
 #if defined __UCLIBC_NTP_LEGACY__
